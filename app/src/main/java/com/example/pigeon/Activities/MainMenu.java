069da5528f446dc6a85b9753cddae2fc2c471d98
Jkim-Hack <johnkim1108@gmail.com>
2019-05-14 02:04:41 -0400

    Added basic layout for the main menu home screen

---
+++ app/src/main/java/com/example/pigeon/Activities/MainMenu.java
@@ -0,0 +1,15 @@
+package com.example.pigeon.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pigeon.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
